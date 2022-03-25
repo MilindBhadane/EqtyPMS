a771cd4d7b7032efa437c687528d810dc1bb5339
MilindBhadane <MilindBhadane@gmail.com>
2022-03-25 13:37:12 +0530

    Initial Commit

---
+++ EqtyPMS.Service/IService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data;
+
+namespace EqtyPMS.Service
+{
+    [ServiceContract]
+    public interface IService
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        [OperationContract]
+        bool ExecuteSQLData(string query);
+
+        [OperationContract]
+        DataSet GetSQLData(string query);
+
+        [OperationContract]
+        DataSet GetAccessDBData(string query, string tableName);
+
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ EqtyPMS.Service/Service.svc.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using EqtyPMS.DAL;
+using System.Data;
+
+namespace EqtyPMS.Service
+{
+    public class Service : IService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public DataSet GetAccessDBData(string query, string tableName)
+        {
+            DataSet dataset = new DataSet();
+            try
+            {
+                DBConnector dbConnector = new DBConnector();
+                dataset = dbConnector.GetAccessDBData(query, tableName);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+
+            }
+            return dataset;
+        }
+
+        public DataSet GetSQLData(string query)
+        {
+            DataSet dataset = new DataSet();
+            try
+            {
+                DBConnector dbConnector = new DBConnector();
+                dataset = dbConnector.GetSQLData(query);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                
+            }
+            return dataset;
+        }
+
+        public bool ExecuteSQLData(string query)
+        {
+            bool retval = false;
+            try
+            {
+                DBConnector dbConnector = new DBConnector();
+                retval = dbConnector.ExecuteNonQuery(query);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                
+            }
+            return retval;
+        }
+
+
+    }
+}
